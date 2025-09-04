c3613a2cdd97ccb0785e58d236738f9ca4b12ad1
tantrnH <92703402+tantrnH@users.noreply.github.com>
2025-09-05 03:13:54 +0800

    Finished mid term question 3

--- MidTermTest.sql
+++ MidTermTest.sql
@@ -11,7 +11,7 @@ CREATE TABLE STUDENT_DIM (
     S_DATE_JOINED             DATE            NOT NULL,
     S_CGPA                    NUMBER(3,2)     NOT NULL,
     S_FACULTY_ENROLLED        VARCHAR2(20)    NOT NULL,
-    S_HIGHTEST_QUALIFICATION  VARCHAR2(15)    NOT NULL,
+    S_HIGHEST_QUALIFICATION   VARCHAR2(15)    NOT NULL,
     CONSTRAINT STUDENT_DIM_PK PRIMARY KEY(S_KEY)
 );
 
@@ -20,9 +20,9 @@ CREATE SEQUENCE STUDENT_DIM_SEQ START WITH 1234 INCREMENT BY 1;
 CREATE TABLE COURSE_DIM (
     COURSE_KEY              NUMBER          NOT NULL,  
     COURSE_ID               NUMBER          NOT NULL,
+    COURSE_TITLE            VARCHAR2(50)    NOT NULL,
     FACULTY_NAME            VARCHAR2(25)    NOT NULL,
     MAX_CAPACITY            NUMBER          NOT NULL,
-    NO_ENROLLED             NUMBER          NOT NULL,
     EFFECTIVE_START_DATE    DATE            NOT NULL,
     EFFECTIVE_END_DATE      DATE            NOT NULL,
     CONSTRAINT COURSE_DIM_PK PRIMARY KEY(COURSE_KEY)
@@ -33,7 +33,7 @@ CREATE SEQUENCE COURSE_DIM_SEQ START WITH 1234 INCREMENT BY 1;
 CREATE TABLE DATE_DIM (
     DATE_KEY            NUMBER          NOT NULL,
     CAL_DATE            DATE            NOT NULL,
-    ACAD_YEAR           NUMBER          NOT NULL,
+    ACAD_YEAR           VARCHAR2(9)     NOT NULL,
     FULL_DESC           VARCHAR2(40),  
     DAY_WEEK            NUMBER(1),
     DAY_NUM_MONTH       NUMBER(2),
@@ -53,30 +53,59 @@ CREATE TABLE DATE_DIM (
 );
 
 CREATE TABLE ENROLLMENT_FACT(
-    S_KEY           NUMBER  NOT NULL,
-    COURSE_KEY      NUMBER  NOT NULL,
-    DATE_KEY        NUMBER  NOT NULL,
-    SE_ID           NUMBER  NOT NULL,
-    ACAD_SESSION    NUMBER(6)   NOT NULL,
+    S_KEY           NUMBER      NOT NULL,
+    COURSE_KEY      NUMBER      NOT NULL,
+    DATE_KEY        NUMBER      NOT NULL,
+    SE_ID           NUMBER      NOT NULL,
+    REGISTER_DATE   DATE        NOT NULL,
     GRADE           VARCHAR2(3) NOT NULL,     
     FEE_PER_CREDIT  NUMBER(6,2) NOT NULL,
-    CREDIT_HOUR     NUMBER(3),
+    CREDIT_HOUR     NUMBER(3)   NOT NULL,  
     TOTAL_FEES      NUMBER(8,2) NOT NULL,
-    PASS_FLAG       CHAR(1),
     CONSTRAINT EF_PK PRIMARY KEY(S_KEY, COURSE_KEY, DATE_KEY, SE_ID),
     CONSTRAINT EF_COURSE_FK FOREIGN KEY(COURSE_KEY) REFERENCES COURSE_DIM(COURSE_KEY),
-    CONSTRAINT EF_DATE_FK FOREIGN KEY(DATE_KEY) REFERENCES DATE_DIM(DATE_KEY),
-    CONSTRAINT EF_S_FK FOREIGN KEY(S_KEY) REFERENCES STUDENT_DIM(S_KEY),
-    CONSTRAINT EF_SE_FK FOREIGN KEY(SE_ID) REFERENCES Semester_Enrollment(SE_ID)
+    CONSTRAINT EF_DATE_FK   FOREIGN KEY(DATE_KEY)   REFERENCES DATE_DIM(DATE_KEY),
+    CONSTRAINT EF_S_FK      FOREIGN KEY(S_KEY)      REFERENCES STUDENT_DIM(S_KEY),
+    CONSTRAINT EF_SE_FK     FOREIGN KEY(SE_ID)      REFERENCES Semester_Enrollment(SE_ID)
 );
 
 /* =================================== 2) UPDATE ACADEMIC YEAR =================================== */
 
 UPDATE DATE_DIM d
-SET acad_year = CASE
+SET ACAD_YEAR = CASE
   WHEN d.CAL_DATE BETWEEN DATE '2025-06-16' AND DATE '2026-04-25' THEN '2025/26'
   WHEN d.CAL_DATE BETWEEN DATE '2026-06-29' AND DATE '2027-04-24' THEN '2026/27'
   ELSE d.ACAD_YEAR
 END;
 
 /* =================================== 3) Initial Loading for Fact Table =================================== */
+
+INSERT INTO ENROLLMENT_FACT (
+  S_KEY, 
+  COURSE_KEY, 
+  DATE_KEY, 
+  SE_ID,
+  REGISTER_DATE, 
+  GRADE, 
+  FEE_PER_CREDIT, 
+  CREDIT_HOUR, 
+  TOTAL_FEES
+)
+SELECT
+  sd.S_KEY,                               
+  cd.COURSE_KEY,                           
+  TO_NUMBER(TO_CHAR(cel.CEL_register_date,'YYYYMMDD')) AS DATE_KEY,
+  se.SE_ID,                                
+  cel.CEL_register_date,
+  cel.CEL_grade,
+  cl.CL_fee_per_credit,
+  c.C_credit_hour,
+  (cl.CL_fee_per_credit * c.C_credit_hour) AS TOTAL_FEES
+FROM   COURSE_ENROLL_LIST   cel                 
+JOIN   SEMESTER_ENROLLMENT  se  ON se.SE_ID         = cel.SE_ID
+JOIN   STUDENT              s   ON s.S_ID           = se.SE_Student_ID
+JOIN   STUDENT_DIM          sd  ON sd.S_ID          = s.S_ID           
+JOIN   COURSE_LIST          cl  ON cl.CL_ID         = cel.CL_ID
+JOIN   COURSE               c   ON c.C_ID           = cl.CL_Course_ID
+JOIN   COURSE_DIM           cd  ON cd.COURSE_ID     = c.C_ID           
+JOIN   DATE_DIM             d   ON d.CAL_DATE       = TRUNC(cel.CEL_register_date);
